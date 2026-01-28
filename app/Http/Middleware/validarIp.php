307b24568c601e77b48f704978c33b72ede19b28
angelgabriel2030 <gabrielcampirano@gmail.com>
2026-01-28 07:30:48 -0600

    Tercer commit

--- app/Http/Middleware/validarIp.php
+++ app/Http/Middleware/validarIp.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-class ValidatePartnerIp
+class validarIp
 {
     private array $allowedIps = [
         '127.0.0.1',
