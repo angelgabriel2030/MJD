6bbff0eed3acb3cfa9343654ef21c4cec22c0a7b
angelgabriel2030 <gabrielcampirano@gmail.com>
2026-01-28 07:39:00 -0600

    Cuarto commit

--- routes/api.php
+++ routes/api.php
@@ -5,11 +5,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware(['validate.partner.ip'])->group(function () {
     
-    Route::post('/juguetes/desde-mascota', [JugueteController::class, 'recibirDesdeMascota']);
-    Route::post('/juguetes/desde-dulces', [JugueteController::class, 'recibirDesdeDulces']);
+    Route::post('/juguetes/desde-mascota', [JuguetesController::class, 'recibirDesdeMascota']);
+    Route::post('/juguetes/desde-dulces', [JuguetesController::class, 'recibirDesdeDulces']);
     
 });
 
-Route::get('/juguetes', [JugueteController::class, 'index']);
-Route::get('/juguetes/{id}', [JugueteController::class, 'show']);
-Route::get('/imagenes', [JugueteController::class, 'listarImagenes']);
+Route::get('/juguetes', [JuguetesController::class, 'index']);
+Route::get('/juguetes/{id}', [JuguetesController::class, 'show']);
+Route::get('/imagenes', [JuguetesController::class, 'listarImagenes']);
